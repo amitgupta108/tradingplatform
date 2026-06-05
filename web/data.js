bd85a680ffcbaf3c6f8c972b08cdc9f37264a03b
Amit Gupta <reach.amit.108@gmail.com>
2026-06-05 08:26:16 +0530

    Option Chain defect fix

--- web/data.js
+++ web/data.js
@@ -8,7 +8,7 @@ instrumentMap.set('UI', {
   oExpiryNxt: "24FEB26",
   stockCode: 'NIFTY',
   exc: 'NFO',
-  lscount: 10,
+  lscount: lscount,
   mode: 2,
   lotsize: 65,
   appid: '431c0a81-0558-4ad7-8b4c-1ea5bf3775b4'
@@ -21,7 +21,7 @@ instrumentMap.set('NH2', {
   oExpiryNxt: "24MAR26",
   stockCode: 'NIFTY',
   exc: 'NFO',
-  lscount: 10,
+  lscount: lscount,
   mode: 0,
   lotsize: 65,
   appid: 'b6033d82-1d88-470a-bf28-58b217f098e7'
@@ -34,7 +34,7 @@ instrumentMap.set('NH1', {
   oExpiryNxt: "09JUN26",
   stockCode: 'NIFTY',
   exc: 'NFO',
-  lscount: 9,
+  lscount: lscount,
   mode: 0,
   lotsize: 65,
   appid: '5be36ca0-44e8-44d9-b739-4864c6dfc553'
@@ -47,7 +47,7 @@ instrumentMap.set('NL1', {
   oExpiryNxt: "16JUN26",
   stockCode: 'NIFTY',
   exc: 'NFO',
-  lscount: 9,
+  lscount: lscount,
   mode: 1,
   lotsize: 65,
   appid: '0cd4a0ed-c4a1-4318-940b-b4d3841468d9'
@@ -60,7 +60,7 @@ instrumentMap.set('NL2', {
   oExpiryNxt: "16JUN26",
   stockCode: 'NIFTY',
   exc: 'NFO',
-  lscount: 9,
+  lscount: lscount,
   mode: 3,
   lotsize: 65,
   appid: '886ee155-38d9-49c1-9e18-b17551e8a4be'
@@ -73,7 +73,7 @@ instrumentMap.set('NP1', {
   oExpiryNxt: "09JUN26",
   stockCode: 'NIFTY',
   exc: 'NFO',
-  lscount: 12,
+  lscount: lscount,
   mode: 2,
   lotsize: 65,
   appid: crypto.randomUUID
@@ -86,7 +86,7 @@ instrumentMap.set('MP1',{
   oExpiryNxt: "16JUN26",
   stockCode: 'CRUDEOIL',
   exc: 'MCX',
-  lscount: 10,
+  lscount: lscount,
   mode: 2,
   lotsize: 100,
   appid: 'sM2wzV0S-x3Ca-SVQI-AAAH-940b886ee155'
@@ -99,7 +99,7 @@ instrumentMap.set('ML1',{
   oExpiryNxt: "16JUL26",
   stockCode: 'CRUDEOIL',
   exc: 'MCX',
-  lscount: 10,
+  lscount: lscount,
   mode: 1,
   lotsize: 100,
   appid: '886ee155-38d9-49c1-9e18-b17551e8a4be'
@@ -112,7 +112,7 @@ instrumentMap.set('ML2',{
   oExpiryNxt: "16JUL26",
   stockCode: 'CRUDEOIL',
   exc: 'MCX',
-  lscount: 10,
+  lscount: lscount,
   mode: 3,
   lotsize: 100,
   appid: '431c0a81-0558-4ad7-8b4c-1ea5bf3775b4'
