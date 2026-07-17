21cb4460772c0dba3155950dd3af151e61b7a433
Amit Gupta <reach.amit.108@gmail.com>
2026-07-17 22:31:38 +0530

    Stability Fix (openalgo)

--- serverlocal/broker/m_t_openalgo.mjs
+++ serverlocal/broker/m_t_openalgo.mjs
@@ -25,9 +25,10 @@ function onQuotes(q)
     const l_appid = qt.stockCode + view_mode;
     streamer.emitQs(l_appid, qt);
     
+    const key = qt.exchange === 'MCX' ? 'futures' : 'index';
     if(qt.key === 'strikex')
         qutils.sendQsToSim(view_mode, qt);
-    else if(qt.key === 'futures' && (counter === 0 || counter++ === 6)) 
+    else if (qt.key === key && (counter === 0 || counter++ === 6)) 
     {
         counter = 1;
         const response = qutils.atmRefresh(logical_view_name, l_appid, qt);
@@ -49,7 +50,6 @@ function exit(appid, sublist)
 
 function startv2(appid, p)
 {
-    const provider_subs = new Subscriptions(logical_view_name);
     const stock_subs = provider_subs.addNewSubscriptions(p.stockCode + view_mode, p);
     const requests = stock_subs.getSubsItemsByKey(['index', 'futures']);
     const st = requests.find((r) => r.key === 'index');
@@ -95,6 +95,17 @@ function subscribe(appid, list, action)
         client.subscribe_ltp(list, onQuotes);
     else 
         client.unsubscribe_ltp(list, onQuotes);
+}
+
+function option_chain(appid, stockCode, key, action)
+{
+    const provider_subs = subs_store_all[logical_view_name];
+    const stock_subs = provider_subs.getSubscriptionsForStockCode(stockCode);
+    const ost = stock_subs.optionChainAction(key, action);
+    if(ost !== undefined) {
+        const subs_action = ost.toStream === true ? 'subs' : 'unsubs';
+        subscribe(appid, ost.strikes, subs_action);
+    }
 }
 
 async function orderbook(appid, stockCode)
@@ -154,9 +165,11 @@ function init()
 {
     if(!initialized)
     {
+        my_subs = new Subscriptions(logical_view_name);
+          
         if(view_mode === undefined)
             view_mode = services.getProviderModeKey(logical_view_name, 'view')?.at(0);
-        
+
         if(!client)
             client = new OpenAlgo(process.env.openalgo_key);
         
